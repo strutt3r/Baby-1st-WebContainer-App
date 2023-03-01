956e28674c81f0bfc9627fd124c421fa3a00d422
strutt3r <shawnwwerner@gmail.com>
2023-03-01 11:22:55 -0600

    Page 3 completed

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ window.addEventListener('load', async () => {
   // Call only once
   webcontainerInstance = await WebContainer.boot();
   await webcontainerInstance.mount(files);
+  const exitCode = await installDependencies();
+  if (exitCode !== 0) {
+    throw new Error('Installation Failed');
+  }
 });
 
 document.querySelector('#app').innerHTML = `
@@ -27,3 +31,17 @@ const iframeEl = document.querySelector('iframe');
 
 /** @type {HTMLTextAreaElement | null} */
 const textareaEl = document.querySelector('textarea');
+
+async function installDependencies() {
+  // Install Dependencies
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  installProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        console.log(data);
+      },
+    })
+  );
+  // Wait for install command to exit
+  return InstallProcess.exit;
+}
