1444d68577e77b76208a8a36888c200f739e6965
strutt3r <shawnwwerner@gmail.com>
2023-03-01 10:24:29 -0600

    Added scaffolding to main.js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import './style.css'
+import './style.css';
 
+document.querySelector('#app').innerHTML = `
+<div class = "container">
+  <div class = "editor">
+    <textarea>A text area am I</textarea>
+  </div>
+  <div class = "preview">
+    <iframe src = "loading.html"></iframme>
+  </div>
+</div>
+`;
